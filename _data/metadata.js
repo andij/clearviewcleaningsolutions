eaf8b72a7bd3c9f8a9d27a3a0eb66444146667c0
Drew Jones <drew.jones@centrica.com>
2023-04-12 21:21:02 +0100

    add base starter files

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "title": "Clearview Cleaning Solutions",
+  "url": "https://clearviewcleaningsolutions.co.uk/",
+  "language": "en",
+  "description": "Your reliable cleaning and support services partner.",
+  "author": {
+    "name": "Drew Jones",
+    "email": "drew@andij.com",
+    "url": "https://andij.com/"
+  }
+}
